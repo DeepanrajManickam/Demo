3518522f8638bb6c8416f7e327edb28c6e56ffbd
DeepanrajManickam <deepan9486@gmail.com>
2024-09-20 18:44:31 +0530

    changed scanner

---
+++ even or odd.java
@@ -0,0 +1,17 @@
+
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+   
+        int num = 4;
+        System.out.print("Enter a number  int : ");
+
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd ");
+    }
+}
