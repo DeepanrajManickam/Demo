11c5115987033ff6e5faaeac33bc4dd6d296465b
DeepanrajManickam <deepan9486@gmail.com>
2024-09-20 18:52:36 +0530

    Merge

--- even or odd.java
+++ even or odd.java
@@ -1,33 +1,19 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-=======
 
->>>>>>> master
 
 public class EvenOdd {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number  int : ");
-
-        int num = reader.nextInt();
-=======
    
         int num = 4;
         System.out.print("Enter a number  int : ");
 
->>>>>>> master
+
 
         if(num % 2 == 0)
             System.out.println(num + " is even");
         else
-<<<<<<< HEAD
+
             System.out.println(num + " is odd");
-=======
-            System.out.println(num + " is odd ");
->>>>>>> master
-    }
+
 }
